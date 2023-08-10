b1166e0106d132273c9631edf98aec9bc4b7ba2d
Anim <47661479+radiant-fleak@users.noreply.github.com>
2023-08-11 00:07:00 +0600

    Create Commands  In Client.cmd

---
+++ Commands  In Client.cmd
@@ -0,0 +1,6 @@
+    3  vi /etc/sysconfig/network-scripts/ifcfg-eth0
+    4  systemctl restart network
+    5  nmclin connection show
+    6  nmcli connection show
+    7  nmtui
+    8  nmcli connection up ens33
